98ad73fa0e819da75f70e131b6389263fa367bc2
datnohand <gabriel@selinschek.com>
2018-05-07 01:47:12 +0200

    Added ledColor and tweaked config

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,7 @@
 var config = module.exports = {};
 
 config.wss = {};
+config.led = {};
 
 // WebSocket config
 config.wss.secure = true;
@@ -8,3 +9,7 @@ config.wss.online = true;
 config.wss.port = '80';
 
 config.name = 'LED Controller'
+
+// LED config
+config.led.brightness = '30'
+config.led.num = 923
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ var config = require('../config/config.js');
 
 // Other Modules
 var fs = require('fs');
+var strip = require('rpi-ws281x-native');
 
 // HTTPS Server for WSS
 var http = require('http');
@@ -19,44 +20,68 @@ app.use(express.static(__dirname + '/public'));
 var WebSocket = require('ws');
 var WebSocketServer = require('ws').Server;
 
-// Port Range for Slaves
 var port = config.port;
+var NUM_LEDS = parseInt(config.led.num)
+var pixelData = new Uint32Array(NUM_LEDS)
 
 var httpServer = http.createServer();
-httpsServer.listen(config.wss.port);
+httpsServer.listen(config.port);
 var wss = new WebSocketServer({ server: httpsServer });
+
+strip.init(NUM_LEDS)
+strip.setBrightness(config.led.brightness)
 
 console.log('Listening on '+config.wss.port);
 
+// Testing
+// ledSpecial(30, 'ambient')
+
 // If the server gets a connection
 wss.on('connection', function(ws, req) {
-    ws.on('message', (msg) => {
+  SendToEveryone({type: 'msg', txt: 'LED Controller - Control your LED\'s from anywhere!'})
 
-      var valid = false;
+  ws.on('message', (msg) => {
+    var valid = false;
 
-      try {
-        var msg = JSON.parse(msg);
-      } catch   (e){
-        ws.send('{"type": "err", "msg": "ERR_SYNTAX"}');
-        ws.terminate();
-      }
-      // If there is no type in the message
-      if (!msg.type) {
+    try {
+      var msg = JSON.parse(msg);
+    } catch   (e){
       ws.send('{"type": "err", "msg": "ERR_SYNTAX"}');
       ws.terminate();
-      }
+    }
 
-      switch (msg.type) {
-        case:
-        break;
-      }
-    });
+    // If there is no type in the message
+    if (!msg.type) {
+    ws.send('{"type": "err", "msg": "ERR_SYNTAX"}');
+    ws.terminate();
+    }
+
+    switch (msg.type) {
+      case 'color':
+        ledColor(msg.bright, msg.color)
+      break;
+      case 'color_man':
+        ledColorMan(msg.bright, msg.r, msg.g, msg.b)
+      break;
+      case 'special':
+        ledSpecial(msg.bright, msg.mode, msg.arg)
+      break;
+    }
+  });
 });
+
+function ledColor(bright = config.led.brightness, color) {
+  strip.brightness = bright
+  for (i = 0; i < config.led.num; i++) {
+    pixelData[i] = color
+  }
+  strip.render(pixelData)
+}
 
 function SendToEveryone(data) {
         wss.clients.forEach(function each(client) {
         if (client.readyState === WebSocket.OPEN) {
-                client.send(data);
+                client.send(JSON.stringify(data));
         }
         });
 }
@@ -64,7 +89,7 @@ function SendToEveryone(data) {
 function SendToEveryoneButOrigin(data, ws) {
         wss.clients.forEach(function each(client) {
                 if (client !== ws && client.readyState === WebSocket.OPEN) {
-                        client.send(data);
+                        client.send(JSON.stringify(data));
                 }
         });
 }
