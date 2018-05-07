8d884f2d60a9e3b9e2145dbf62831f2811f5d7c9
datnohand <gabriel@selinschek.com>
2018-05-07 10:37:39 +0200

    added 'public' dir, unfinished

---
+++ server/public/client.js
@@ -0,0 +1,118 @@
+function led_color(color) {
+  br = $('#br').val();
+
+  $.ajax({
+  type: "POST",
+  url: '/api.php',
+  dataType: 'json',
+  data: {
+      func: 'led_color',
+      color: color,
+      bright: br
+  },
+  });
+}
+
+function led_off() {
+  $.ajax({
+  type: "POST",
+  url: '/api.php',
+  dataType: 'json',
+  data: {
+      func: 'led_off',
+  },
+  });
+}
+
+function led_rainbow() {
+  br = $('#br').val();
+
+  $.ajax({
+  type: "POST",
+  url: '/api.php',
+  dataType: 'json',
+  data: {
+      func: 'led_rainbow',
+      bright: br
+  },
+  });
+}
+
+function led_epilepsy() {
+  br = $('#br').val();
+
+  $.ajax({
+  type: "POST",
+  url: '/api.php',
+  dataType: 'json',
+  data: {
+      func: 'led_epilepsy',
+      bright: br
+  },
+  });
+}
+
+function led_color_man() {
+  red = $('#red').val();
+  green = $('#green').val();
+  blue = $('#blue').val();
+
+  br = $('#br').val();
+
+  $.ajax({
+  type: "POST",
+  url: '/api.php',
+  dataType: 'json',
+  data: {
+      func: 'led_color_man',
+      red: red,
+      green: green,
+      blue: blue,
+      bright: br
+  },
+  });
+}
+
+function led_color_hex() {
+  hex = $('#hex').val();
+  br = $('#br').val();
+
+  r = hexToRgb(hex).r;
+  g = hexToRgb(hex).g;
+  b = hexToRgb(hex).b;
+
+  $.ajax({
+  type: "POST",
+  url: '/api.php',
+  dataType: 'json',
+  data: {
+      func: 'led_color_man',
+      red: red,
+      green: green,
+      blue: blue
+  },
+  });
+}
+
+function led_ambient() {
+  br = $('#br').val();
+
+  $.ajax({
+  type: "POST",
+  url: '/api.php',
+  dataType: 'json',
+  data: {
+      func: 'led_ambient',
+      bright: br
+  },
+  });
+}
+
+function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
